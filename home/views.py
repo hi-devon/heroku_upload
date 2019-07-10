7e058351a65df1b7fbc39d3ef57d61378f91f21f
devon <51902979+hi-devon@users.noreply.github.com>
2019-07-10 14:24:14 +0900

    contact modify

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import datetime
 from home.models import Board, Contact
+from _ast import Or
 #-*- coding: utf-8 -*-
 # Create your views here.
 def home(request):
@@ -14,13 +15,17 @@ def board_list(request):
 
 @csrf_exempt
 def contact(request):
-    cont = Contact.objects.create(
-            customer_name = nullcheck(request.POST.get('customer_name')),
-            customer_email = nullcheck(request.POST.get('email')),
-            title = nullcheck(request.POST.get('title')),
-            content = nullcheck(request.POST.get('content')),
-            reg_date = datetime.date.today()
-            )
+    if nullcheck(request.POST.get('customer_name')) != None and \
+        nullcheck(request.POST.get('email')) != None and \
+        nullcheck(request.POST.get('content')) != None:
+    
+        cont = Contact.objects.create(
+                customer_name = nullcheck(request.POST.get('customer_name')),
+                customer_email = nullcheck(request.POST.get('email')),
+                title = nullcheck(request.POST.get('title')),
+                content = nullcheck(request.POST.get('content')),
+                reg_date = datetime.date.today()
+                )
     return render(request, 'contact.html')
 
 def nullcheck(val):
